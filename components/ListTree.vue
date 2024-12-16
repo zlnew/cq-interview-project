35a9d778079e25fb281578dd25a619060e37cfcd
Maulana Aprizqy Sumaryanto <aprizqy.dev@gmail.com>
2024-12-16 13:34:27 +0700

    show user data dynamicly

---
+++ components/ListTree.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  data: object
+  child?: boolean
+}>()
+</script>
+
+<template>
+  <ul class="group divide-y">
+    <li
+      v-for="(value, key) in data"
+      :key="key"
+      class="grid grid-cols-5 gap-2 divide-x"
+      :class="child ? 'ml-4' : 'ml-0'"
+    >
+      <template v-if="typeof value !== 'object'">
+        <div class="font-bold capitalize">{{ key }}</div>
+        <div class="col-span-4">{{ value }}</div>
+      </template>
+
+      <template v-else-if="value !== null">
+        <ul class="col-span-5 divide-y">
+          <div class="bg-slate-200 px-2 font-bold capitalize">{{ key }}</div>
+          <ListTree
+            :data="value"
+            child
+          />
+        </ul>
+      </template>
+    </li>
+  </ul>
+</template>
